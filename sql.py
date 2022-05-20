b29e01f54a45c8c10bf9ae0cc1a3a4e42c241f0b
Prabhu Patnala <73421334+plrcprabhu@users.noreply.github.com>
2022-05-20 17:46:41 +0530

    Update sql.py

--- sql.py
+++ sql.py
@@ -4,13 +4,13 @@ import sqlite3
 conn=sqlite3.connect('Restaurant_food_data.db')
 c=conn.cursor()
 
-#c.execute("CREATE TABLE item (Item_name text,No_of_customers text,No_of_positive_reviews text,No_of_negative_reviews text,Positive_percentage text,Negative_percentage text) ")
+c.execute("CREATE TABLE item (Item_name text,No_of_customers text,No_of_positive_reviews text,No_of_negative_reviews text,Positive_percentage text,Negative_percentage text) ")
 
-#c.execute("CREATE TABLE limitt (Food_item text, Cust_limit text)")
+c.execute("CREATE TABLE limitt (Food_item text, Cust_limit text)")
 
-#c.execute("CREATE TABLE reviewdata (Item_review text, Status text, Freq_cost text)")
+c.execute("CREATE TABLE reviewdata (Item_review text, Status text, Freq_cost text)")
 
-#c.execute("CREATE TABLE freqdata (Word text, Count text)")
+c.execute("CREATE TABLE freqdata (Word text, Count text)")
 
 
 conn.commit()
